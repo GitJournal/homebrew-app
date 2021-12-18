da18c9d864c0a695531dc45c8071326434efad5f
Vishesh Handa <me@vhanda.in>
2021-12-18 12:57:31 +0100

    Style fixes

--- Casks/gitjournal.rb
+++ Casks/gitjournal.rb
@@ -1,19 +1,18 @@
 cask "gitjournal" do
   version "1.80.0"
-
   sha256 "90c3b0db9f31ca3e1b12638d4e7b20485a14820d41fa99b12f33ce7b4fab9c2b"
+
   url "https://github.com/GitJournal/GitJournal/releases/download/v#{version}/GitJournal-osx.dmg",
       verified: "github.com/GitJournal/GitJournal"
-
   name "GitJournal"
   desc "Mobile first notes integrated with Git"
-  homepage "https://gitjournal.io"
+  homepage "https://gitjournal.io/"
 
-  #livecheck do
-    # FIXME: Fix this url!
+  # livecheck do
+  # FIXME: Fix this url!
   #  url "https://storage.googleapis.com/flutter_infra_release/releases/releases_macos.json"
   #  regex(%r{/flutter[._-]macos[._-]v?(\d+(?:\.\d+)+)[._-]stable\.zip}i)
-  #end
+  # end
 
   app "GitJournal.app"
   # container type:dmg
