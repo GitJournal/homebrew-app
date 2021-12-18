93e1ee5a0f5cd101e0c0db1f7f9d6556b5f52d88
Vishesh Handa <me@vhanda.in>
2021-12-18 12:48:23 +0100

    Add verified to the download URL

--- Casks/gitjournal.rb
+++ Casks/gitjournal.rb
@@ -1,8 +1,9 @@
 cask "gitjournal" do
   version "1.80.0"
 
-  sha256 "90c3b0db9f31ca3e1b12638d4e7b20485a14820d41fa99b12f33ce7b4fab9c2b" 
-  url "https://github.com/GitJournal/GitJournal/releases/download/v#{version}/GitJournal-osx.dmg"
+  sha256 "90c3b0db9f31ca3e1b12638d4e7b20485a14820d41fa99b12f33ce7b4fab9c2b"
+  url "https://github.com/GitJournal/GitJournal/releases/download/v#{version}/GitJournal-osx.dmg",
+      verified: "github.com/GitJournal/GitJournal"
 
   name "GitJournal"
   desc "Mobile first notes integrated with Git"
